858bd17ba8538a630a62223b567a25d8a5ab3dda
Maesfetti <justbestartem@mail.ru>
2024-04-06 02:12:27 +0300

    6 practice - notifications

--- app/src/main/java/com/example/myapp4/FirstFragment.java
+++ app/src/main/java/com/example/myapp4/FirstFragment.java
@@ -1,20 +1,53 @@
 package com.example.myapp4;
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.Manifest;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.ListFragment;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-public class FirstFragment extends Fragment {
+import android.widget.Button;
 
+public class FirstFragment extends Fragment {
+    private final String ID = "channelID";
+    private final int notificationId = 100;
     public FirstFragment() {
         super(R.layout.fragment_first);
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Канал";
+            String description = "Описание";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = (NotificationManager)requireContext()
+                    .getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_first, container, false);
 
@@ -35,6 +68,25 @@ public class FirstFragment extends Fragment {
                 String result = "Данные, переданные из StartFragment в RecyclerFragment";
                 bundle.putString("recyclerKey", result);
                 Navigation.findNavController(v).navigate(R.id.action_FirstFrag_to_recyclerFragment, bundle);
+            }
+        });
+
+        view.findViewById(R.id.notificationButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(requireContext(), ID)
+                        .setSmallIcon(R.drawable.ic_launcher_foreground)
+                        .setContentTitle("BusinessAnalysis")
+                        .setContentText("There is new information about companies")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(requireContext());
+                if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED)
+                    return;
+                Log.i("needed button", "gotovo");
+                notificationManager.notify(notificationId, builder.build());
+
             }
         });
 
